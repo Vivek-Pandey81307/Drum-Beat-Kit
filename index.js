2d7a45d567f3f73fdea2eee5393bbd2243d061b7
Vivek Kumar Pandey <vs9595@srmist.edu.in>
2023-10-27 22:06:16 +0530

    Final commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var numberofdrums=document.querySelectorAll(".drum").length;
+for(var i=0;i<numberofdrums;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        this.style.color="white";
+        animation(this.innerHTML)
+        makesound(this.innerHTML); 
+    });
+}
+document.addEventListener("keydown",function(et){
+    animation(et.key)
+    makesound(et.key);
+})
+function makesound(k){
+    switch(k){
+        case "w":
+            var audio1= new Audio("sounds/crash.mp3");
+            audio1.play();break;
+        case "a":
+            var audio2 = new Audio("sounds/kick-bass.mp3");
+            audio2.play();break;
+        case "s":
+            var audio3 = new Audio("sounds/snare.mp3");
+            audio3.play();break;
+        case "d":
+            var audio4 = new Audio("sounds/tom-1.mp3");
+            audio4.play();break;
+        case "j":
+            var audio5 = new Audio("sounds/tom-2.mp3");
+            audio5.play();break;
+        case "k":
+            var audio6 = new Audio("sounds/tom-3.mp3");
+            audio6.play();break;
+        case "l":
+            var audio7 = new Audio("sounds/tom-4.mp3");
+            audio7.play();break;
+        default:console.log("buttonn")
+    }
+
+}
+function animation(key){
+    document.querySelector("."+ key).classList.add("pressed");
+    setTimeout(function(){document.querySelector("."+ key).classList.remove("pressed");},100);
+}
